882ac7485fc34c7494d13b2b55dc58478f59e7aa
Ballad <junjielu1008@gmail.com>
2022-04-18 15:26:19 +0800

    fix: layer color

--- SkeletonViewCore/Sources/Internal/UIKitExtensions/UIView+Extensions.swift
+++ SkeletonViewCore/Sources/Internal/UIKitExtensions/UIView+Extensions.swift
@@ -143,6 +143,12 @@ extension UIView {
             // Workaround to fix the problem when inserting a sublayer and
             // the content offset is modified by the system.
             (self as? UITextView)?.setContentOffset(.zero, animated: false)
+
+            // Update the color after insertion
+            // Since the system needs to derive the color based on the view hierarchy
+            // otherwise, the color is compatible with system color scheme.
+            // see https://github.com/Juanpe/SkeletonView/issues/392
+            skeletonLayer.update(usingColors: config.colors)
             
             if config.animated {
                 self.startSkeletonAnimation(config.animation)
